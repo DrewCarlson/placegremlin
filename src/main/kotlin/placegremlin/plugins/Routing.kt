0fa435662c947a0cceae9cc38aefbc4e1d0e3c15
Drew Carlson <awcarlson@protonmail.com>
2023-07-25 17:32:20 -0700

    Simplify image route and handle various errors

--- src/main/kotlin/placegremlin/plugins/Routing.kt
+++ src/main/kotlin/placegremlin/plugins/Routing.kt
@@ -14,45 +14,54 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
+private val IMAGE_SIZE_RANGE = 1..5000
+
 fun Application.configureRouting() {
     install(AutoHeadResponse)
     val imagesUri = System.getenv("IMAGE_PATH").orEmpty().ifBlank {
         checkNotNull(this::class.java.classLoader.getResource("gremlins")?.file)
     }
+    val imageFiles = File(imagesUri).listFiles()
 
     routing {
         get("/") {
             call.respondText("Load images with https://placegreml.in/{width}/{height}/image.jpg\nExample: https://placegreml.in/200/200/image.jpg")
         }
-        route("/{width}/{height}/image.jpg") {
-            handle {
-                val width = call.parameters["width"]?.toIntOrNull()
-                val height = call.parameters["height"]?.toIntOrNull()
+        get("/{width}/{height}/image.jpg") {
+            val width = call.parameters["width"]?.toIntOrNull()
+            val height = call.parameters["height"]?.toIntOrNull()
 
-                if (width == null || height == null) {
-                    call.respond(BadRequest, "Both width and height must be provided and must be integers")
-                    return@handle
-                }
+            if ((width == null || height == null) || (width !in IMAGE_SIZE_RANGE || height !in IMAGE_SIZE_RANGE)) {
+                call.respond(BadRequest, "Both width and height must be provided and must be integers within $IMAGE_SIZE_RANGE")
+                return@get
+            }
 
-                val files = File(imagesUri).listFiles()
-                if (files.isNullOrEmpty()) {
-                    call.respond(InternalServerError, "No images found")
-                    return@handle
-                }
+            if (imageFiles.isNullOrEmpty()) {
+                call.respond(InternalServerError, "No images found")
+                return@get
+            }
 
-                val randomFile = files.random()
-                call.application.log.debug("Serving image: ${randomFile.absolutePath}")
-                var image: BufferedImage? = ImageIO.read(randomFile)
+            val randomFile = imageFiles.random()
+            call.application.log.debug("Serving image: ${randomFile.absolutePath}")
 
-                checkNotNull(image) {
-                    "Failed to load image: ${randomFile.absolutePath}"
-                }
+            var image: BufferedImage? = try {
+                checkNotNull(ImageIO.read(randomFile))
+            } catch (e: Throwable) {
+                call.application.log.error("Failed to load image file: ${randomFile.absolutePath}", e)
+                call.respond(InternalServerError, "Failed to load image file")
+                return@get
+            }
 
-                image = Scalr.resize(image, QUALITY, FIT_EXACT, width, height)
+            image = try {
+                checkNotNull(Scalr.resize(image, QUALITY, FIT_EXACT, width, height))
+            } catch (e: Throwable) {
+                call.application.log.error("Error while processing image", e)
+                call.respond(InternalServerError, "Failed to process and resize image")
+                return@get
+            }
 
-                call.respondOutputStream(ContentType.Image.JPEG) {
-                    ImageIO.write(image, "jpg", this)
-                }
+            call.respondOutputStream(ContentType.Image.JPEG) {
+                ImageIO.write(image, "jpg", this)
             }
         }
     }
